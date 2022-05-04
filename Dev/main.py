f15ed355e51f6559d602078f6e71dd220e42cc4d
JoaoMorais96 <goncalomorais96@hotmail.com>
2022-05-04 09:15:46 +0100

    Simplified

--- Dev/main.py
+++ Dev/main.py
@@ -1,30 +1,9 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
-from flask_sqlalchemy import SQLAlchemy
 
-import os
-if os.path.exists("./Dev/database.db"):
-	os.remove("./Dev/database.db")#remove existing database. Only usefull in debugging, has to be removed for production version
 
 app = Flask(__name__)
 api = Api(app)
-#define the location of our database. database.db is the User_ID of the db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database.db'
-db = SQLAlchemy(app)
-
-#Define the schema/model of the database entries
-class FormModel(db.Model):
-	UI = db.Column(db.String(100), primary_key=True)#primary_key means that this parameter is unique for each entry
-	User_ID = db.Column(db.String(100), nullable=False)#nullable=False: means that this parameter has always to be filled in an entry
-	Quiz_ID = db.Column(db.String(100), nullable=False)
-	Reward = db.Column(db.Integer, nullable=False)
-	Completed = db.Column(db.Boolean, nullable=False)
-
-	def __repr__(self):
-		return f"Form(User_ID = {self.User_ID}, Quiz_ID = {self.Quiz_ID}, Reward = {self.Reward})"
-
-#Create the database
-db.create_all()
 
 #Validate the request: it has to have the following args when calling a PUT request
 form_put_args = reqparse.RequestParser()
@@ -36,70 +15,13 @@ form_update_args = reqparse.RequestParser()
 form_update_args.add_argument("Reward", type=int, help="Reward payed")
 form_update_args.add_argument("Completed", type=bool, help="Quiz Completed")
 
-#Defines how the object will be returned/serialized (JSON in this case). Mimics the Model/Schema of the database
-resource_fields = {
-	'UI': fields.String,
-	'User_ID': fields.String,
-	'Quiz_ID': fields.String,
-	'Reward': fields.Integer,
-	'Completed': fields.Boolean
-}
-
 class Form(Resource):
-	@marshal_with(resource_fields)#specifies that the "result" value will be displayed/serialized in the way described in resource_fields 
-	def get(self, form_id):
-		result = FormModel.query.filter_by(UI=form_id).first()
-		if not result:
-			abort(404, message="Could not find form with that id")
-		return result
-
-	@marshal_with(resource_fields)
-	def put(self, form_id):
-		args = form_put_args.parse_args()
-
-		result = FormModel.query.filter_by(UI=form_id).first()#Makes a GET request with the vid_id we want to POST
-															    #we have to see if we want just the first() or .all()
-		if result:#If there is a result, then the form was already posted
-			abort(409, message="Form already exists")
-
-		#Make the formID aka UI
-		form_id = args['User_ID'] + args['Quiz_ID']
-		#Otherwise, we can go ahead and post the form by reading the request args
-		form = FormModel(UI=form_id, User_ID=args['User_ID'], Quiz_ID=args['Quiz_ID'], Reward=0, Completed=False)
-		#Add the form to the database
-		db.session.add(form)#adds object to the DB session (temporary)
-		db.session.commit()#commits the changes permanetly to the DB
-
-		#Return the POST info and the correct operation code
-		return form, 201
-
-	@marshal_with(resource_fields)
-	def patch(self, form_id):
-		args = form_update_args.parse_args()
-		result = FormModel.query.filter_by(UI=form_id).first()
-		if not result:
-			abort(404, message="form doesn't exist, cannot update")
-
-		if args['Reward']:
-			result.Reward = args['Reward']
-		if args['Completed']:
-			result.Completed = args['Completed']
-
-		db.session.commit()
-
-		return result
-
-	@marshal_with(resource_fields)
-	def delete(self, form_id):
-		args = form_update_args.parse_args()
-		result = FormModel.query.filter_by(UI=form_id).first()
-		if not result:
-			abort(404, message="form doesn't exist, cannot update")
-		#del forms[form_id]
-		return '', 204
+	
+	def get(self, wallet_id, reward):
+		return {"wallet_id": wallet_id, "reward": reward}
 
 
-api.add_resource(Form, "/form/<string:form_id>")
+api.add_resource(Form, "/form/<string:wallet_id>/<int:reward>")
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- Dev/test.py
+++ Dev/test.py
@@ -2,19 +2,7 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [{"UI": "1Yo1","User_ID": "1", "Quiz_ID": "Yo1", "Reward": 0, "Completed": False},
-        {"UI": "2Yo2","User_ID": "2", "Quiz_ID": "Yo2", "Reward": 0, "Completed": False},
-        {"UI": "3Yo3","User_ID": "3", "Quiz_ID": "Yo3", "Reward": 0, "Completed": False},
-        {"UI": "4Yo4","User_ID": "4", "Quiz_ID": "Yo4", "Reward": 0, "Completed": False}]
-
-for i in range(len(data)):
-    form_ID = data[i]["User_ID"] + data[i]["Quiz_ID"]
-    #We store this info in base + "The user ID" +"/"+ "the formID". This way its easy to query what each user has done
-    #print(BASE + "form/"+data[i]["User_ID"]+"/" + form_ID, data[i])
-    response = requests.put(BASE +"form/" + form_ID, data[i])
-    print(response.json())
-
 input()
 
-response = requests.get(BASE + "form/2Yo2")
+response = requests.get(BASE + "form/0x873648292jei283/32")
 print(response.json())
