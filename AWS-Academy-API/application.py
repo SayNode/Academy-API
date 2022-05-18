41181b3c7396b4e3b37d7e42e3c1a090e479c4b3
JoaoMorais96 <goncalomorais96@hotmail.com>
2022-05-18 11:02:02 +0100

    more changes for https

--- AWS-Academy-API/application.py
+++ AWS-Academy-API/application.py
@@ -1,6 +1,7 @@
 from email.mime import application
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from decouple import config
 import vechain_txs 
 
 application = Flask(__name__)
@@ -8,18 +9,20 @@ api = Api(application)
 
 @application.route('/')
 def index():
-    return 'Hello'
-    
+    return 'Unidentified API'
+   
 class Form(Resource):
 	
-	def get(self, wallet_id, reward):
-
+	def get(self, wallet_id, reward, api_key):
+		PRIVATE_KEY = config('PRIVATE_KEY')
+		if PRIVATE_KEY != api_key:
+			abort(401, message="Wrong API key")
 		vechain_txs.main(wallet_id,reward)
 		return reward+" DHN tokens were awarded to the following wallet address: "+ wallet_id
 
 
-api.add_resource(Form, "/form/<string:wallet_id>/<string:reward>")
+api.add_resource(Form, "/form/<string:wallet_id>/<string:reward>/<string:api_key>")
 
 if __name__ == "__main__":
 
-	application.run(ssl_context=("cert.pem", "key.pem"))
+	application.run()
