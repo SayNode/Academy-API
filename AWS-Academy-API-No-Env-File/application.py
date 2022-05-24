09aa8c7d9da513a49b3618b61270792f1566a57a
JoaoMorais96 <goncalomorais96@hotmail.com>
2022-05-24 15:21:21 +0100

    adding status in json format

--- AWS-Academy-API-No-Env-File/application.py
+++ AWS-Academy-API-No-Env-File/application.py
@@ -1,9 +1,11 @@
 from email.mime import application
 from flask import Flask
+from flask import jsonify
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from decouple import config
 import vechain_txs 
 import os
+
 
 application = Flask(__name__)
 api = Api(application)
@@ -19,15 +21,20 @@ class Form(Resource):
 		if PRIVATE_KEY != api_key:
 			abort(401, message="Wrong API key")
 		vechain_txs.main(wallet_id,reward)
-		return reward+" DHN tokens were awarded to the following wallet address: "+ wallet_id
+		return jsonify(
+				response_one = reward+" DHN tokens were awarded to the following wallet address: "+ wallet_id,
+				status = "200"
+			   )
 
 class Balances(Resource):
 	def get(self, wallet_id_one, wallet_id_two):
 		(balance_one, balance_two)=vechain_txs.balances(wallet_id_one, wallet_id_two)
-		return "Wallet1("+wallet_id_one+") DHN balance:"+ str(balance_one) + \
-			   "                         Wallet2("+wallet_id_two+") DHN balance:"+str(balance_two)
+		return jsonify(
+				response_one = "Wallet1 ("+wallet_id_one+") DHN balance: "+ str(balance_one/(10**18)),
+		 		response_two = "Wallet2 ("+wallet_id_two+") DHN balance: "+str(balance_two/(10**18)),
+				status = "200"
+			   )
 		
-
 api.add_resource(Form, "/form/<string:wallet_id>/<string:reward>/<string:api_key>")
 api.add_resource(Balances, "/balances/<string:wallet_id_one>/<string:wallet_id_two>")
 
